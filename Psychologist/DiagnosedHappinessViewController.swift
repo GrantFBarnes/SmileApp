1df3c42f955aacc329ba934b0375e0162c1c702a
Grant Barnes <gfbarnes78@gmail.com>
2016-03-08 09:17:08 -0600

    Initial Commit

---
+++ Psychologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psychologist
+//
+//  Created by Grant Barnes on 3/7/16.
+//  Copyright © 2016 Grant Barnes. All rights reserved.
+//
+
+import UIKit
+
+class DiagnosedHappinessViewController: HappinessViewController, UIPopoverPresentationControllerDelegate {
+    
+    override var happiness: Int {
+        didSet {
+            if diagnosticHistory.count < 10 {
+                diagnosticHistory += [happiness]
+            } else {
+                diagnosticHistory += [happiness]
+                diagnosticHistory.removeFirst()
+            }
+        }
+    }
+    
+    
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var diagnosticHistory: [Int] {
+        get { return defaults.objectForKey(History.DefaultsKey) as? [Int] ?? [] }
+        set { defaults.setObject(newValue, forKey: History.DefaultsKey) }
+    }
+    
+    private struct History {
+        static let SegueIdentifier = "Show Diagnostic History"
+        static let DefaultsKey = "DiagnosedHappinessViewController.History"
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case History.SegueIdentifier:
+                if let tvc = segue.destinationViewController as? TextViewController {
+                    if let ppc = tvc.popoverPresentationController {
+                        ppc.delegate = self
+                    }
+                    tvc.text = "\(diagnosticHistory)"
+                }
+            default: break
+            }
+        }
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.None
+    }
+    
+}
